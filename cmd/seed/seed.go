1946f87a8ec67ac84d612edf6f25c913e1c2aaa9
SornchaiTheDev <sornchaithedev@gmail.com>
2025-05-09 18:05:32 +0700

    chore: update seed code

--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/SornchaiTheDev/cs-lab-backend/configs"
+	"github.com/SornchaiTheDev/cs-lab-backend/domain/models"
 	"github.com/SornchaiTheDev/cs-lab-backend/domain/services"
 	"github.com/SornchaiTheDev/cs-lab-backend/internal/adapters/sqlx"
 	"github.com/SornchaiTheDev/cs-lab-backend/internal/requests"
@@ -19,11 +20,13 @@ func main() {
 	userRepo := sqlx.NewSqlxUserRepository(db)
 	userService := services.NewUserService(userRepo)
 
-	user, err := userService.Create(context.Background(), &requests.User{
-		Username:    "SornchaiTheDev",
-		Email:       "sornchaithedev@gmail.com",
-		DisplayName: "Sornchai Somsakul",
-		Roles:       []string{"admin"},
+	user, err := userService.Create(context.Background(), models.UserTypeOauth, &requests.CreateUser{
+		BaseUser: requests.BaseUser{
+			Username:    "SornchaiTheDev",
+			DisplayName: "Sornchai Somsakul",
+			Roles:       []string{"admin"},
+		},
+		Email: "sornchaithedev@gmail.com",
 	})
 	if err != nil {
 		fmt.Println("❌ Error creating user:", err)
