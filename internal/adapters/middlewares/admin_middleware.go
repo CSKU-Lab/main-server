d52d60f29f25671fff4c0a2d12836b41079f7796
SornchaiTheDev <sornchaithedev@gmail.com>
2025-07-26 21:20:44 +0700

    chore: update admin middleware

--- internal/adapters/middlewares/admin_middleware.go
+++ internal/adapters/middlewares/admin_middleware.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"slices"
+
 	"github.com/SornchaiTheDev/cs-lab-backend/constants"
 	"github.com/SornchaiTheDev/cs-lab-backend/domain/cserrors"
 	"github.com/SornchaiTheDev/cs-lab-backend/domain/models"
@@ -10,10 +12,8 @@ import (
 func AdminMiddleware(c *fiber.Ctx) error {
 	user := c.Locals("user").(*models.User)
 
-	for _, role := range user.Roles {
-		if role == constants.ADMIN {
-			return c.Next()
-		}
+	if slices.Contains(user.Roles, constants.ADMIN) {
+		return c.Next()
 	}
 
 	return cserrors.New(cserrors.UNAUTHORIZED, "Unauthorized")
