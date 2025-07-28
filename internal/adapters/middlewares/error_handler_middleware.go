96e5904afbb00fd10677e9bd6e3b79613d7208ff
SornchaiTheDev <sornchaithedev@gmail.com>
2025-07-28 15:39:26 +0700

    chore: remove code from error response

--- internal/adapters/middlewares/error_handler_middleware.go
+++ internal/adapters/middlewares/error_handler_middleware.go
@@ -23,7 +23,6 @@ func (e *errorHandlerMiddleware) ErrorHandler(c *fiber.Ctx, err error) error {
 	var csErr *cserrors.Error
 	if errors.As(err, &csErr) {
 		return c.Status(int(csErr.Code)).JSON(fiber.Map{
-			"code":  csErr.Code,
 			"error": csErr.Message,
 		})
 	}
@@ -35,13 +34,11 @@ func (e *errorHandlerMiddleware) ErrorHandler(c *fiber.Ctx, err error) error {
 
 	if errors.Is(err, fiber.ErrMethodNotAllowed) {
 		return c.Status(fiber.StatusMethodNotAllowed).JSON(fiber.Map{
-			"code":  "METHOD_NOT_ALLOWED",
 			"error": "Method Not Allowed",
 		})
 	}
 
 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-		"code":  "INTERNAL_SERVER_ERROR",
 		"error": "Internal Server Error",
 	})
 }
