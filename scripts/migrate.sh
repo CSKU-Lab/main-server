6177f8400d0577da69cfef83a8a31c5bc9f1990d
SornchaiTheDev <sornchaithedev@gmail.com>
2025-11-24 13:36:12 +0700

    chore: update migrate.sh script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-source ./scripts/env_loader.sh
-
-load_env_context .
-
 atlas schema apply \
 	--url $DATABASE_URL \
-	--to "file://atlas/schema.hcl"
+	--to "file://atlas/schema.hcl" \
+	"$@"
