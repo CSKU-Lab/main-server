969999b1ff33572971b57a642dbfc486bd670499
xeff09 <wongchaita03@gmail.com>
2025-12-13 17:43:10 +0700

    add: basic crud for labs route

--- domain/repositories/lab_repository.go
+++ domain/repositories/lab_repository.go
@@ -5,9 +5,14 @@ import (
 
 	"github.com/CSKU-Lab/main-server/domain/models"
 	"github.com/CSKU-Lab/main-server/internal/requests"
+	"github.com/CSKU-Lab/main-server/internal/sanitize"
 )
 
 type LabRepository interface {
 	GetByID(ctx context.Context, labID string) (*models.Lab, error)
 	Create(ctx context.Context, id string, req *requests.CreateLab, userID string) error
+	GetPagination(ctx context.Context, page int, limit int, search string, sortBy string, sortOrder string, filters []sanitize.Filter) ([]models.Lab, error)
+	Count(ctx context.Context, search string, filters []sanitize.Filter) (int, error)
+	UpdateByID(ctx context.Context, labID string, req *requests.BaseUpdateLab) error
+	DeleteByID(ctx context.Context, labID string) error
 }
--- internal/adapters/rest/routes/cms_lab_routes.go
+++ internal/adapters/rest/routes/cms_lab_routes.go
@@ -1,6 +1,12 @@
 package routes
 
 import (
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/CSKU-Lab/main-server/domain/cserrors"
 	"github.com/CSKU-Lab/main-server/domain/models"
 	"github.com/CSKU-Lab/main-server/domain/services"
 	"github.com/CSKU-Lab/main-server/internal/adapters/middlewares"
@@ -39,5 +45,72 @@ func NewCMSLabRoutes(router fiber.Router, labService services.LabService) {
 		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 			"id": labID,
 		})
+	})
+
+	labRouter.Get("/", middlewares.RBACMiddleware([]models.Role{
+		models.ADMIN,
+		models.INSTRUCTOR,
+	}), func(c *fiber.Ctx) error {
+		pageQuery := c.Query("page", "1")
+		pageSizeQuery := c.Query("page_size", "10")
+		search := c.Query("search", "")
+		sortBy := c.Query("sort_by", "display_name")
+		sortOrder := c.Query("sort_order", "desc")
+
+		filterParams := make(map[string]string)
+		for key, value := range c.Queries() {
+			if strings.Contains(key, "__") {
+				filterParams[key] = value
+			}
+		}
+
+		page, err := strconv.Atoi(pageQuery)
+		if err != nil {
+			return cserrors.New(&cserrors.Option{HttpStatus: http.StatusBadRequest, Message: "Invalid page"})
+		}
+
+		pageSize, err := strconv.Atoi(pageSizeQuery)
+		if err != nil {
+			return cserrors.New(&cserrors.Option{HttpStatus: http.StatusBadRequest, Message: "Invalid page size"})
+		}
+
+		labs, err := labService.GetPagination(c.Context(), page, pageSize, search, sortBy, sortOrder, filterParams)
+		if err != nil {
+			return err
+		}
+
+		count, err := labService.Count(c.Context(), search, filterParams)
+		if err != nil {
+			return cserrors.New(&cserrors.Option{HttpStatus: http.StatusInternalServerError, Message: "Error getting labs count"})
+		}
+
+		return c.JSON(fiber.Map{
+			"pagination": fiber.Map{
+				"page":       page,
+				"total_page": math.Ceil(float64(count/pageSize) + 1),
+				"total_rows": count,
+			},
+			"data": labs,
+		})
+	})
+
+	labRouter.Patch("/:labID", middlewares.RBACMiddleware([]models.Role{
+		models.ADMIN,
+		models.INSTRUCTOR,
+	}), middlewares.ValidateMiddleware[requests.BaseUpdateLab](), func(c *fiber.Ctx) error {
+		user := c.Locals("user").(*models.User)
+		labID := c.Params("labID")
+		req := c.Locals("body").(*requests.BaseUpdateLab)
+
+		return labService.UpdateByID(c.Context(), labID, user.ID, req)
+	})
+
+	labRouter.Delete("/:labID", middlewares.RBACMiddleware([]models.Role{
+		models.ADMIN,
+		models.INSTRUCTOR,
+	}), func(c *fiber.Ctx) error {
+		user := c.Locals("user").(*models.User)
+		labID := c.Params("labID")
+		return labService.DeleteByID(c.Context(), labID, user.ID)
 	})
 }
