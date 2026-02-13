6540140c662e2330a458133950b3da47902ea159
xeff09 <wongchaita03@gmail.com>
2026-02-13 17:05:34 +0700

    add: crud for configs runner and comapre

--- internal/adapters/rest/routes/cms_config_routes.go
+++ internal/adapters/rest/routes/cms_config_routes.go
@@ -8,11 +8,57 @@ import (
 	"github.com/CSKU-Lab/main-server/domain/cserrors"
 	"github.com/CSKU-Lab/main-server/domain/models"
 	configPB "github.com/CSKU-Lab/main-server/genproto/config/v1"
+	"github.com/CSKU-Lab/main-server/internal/adapters/middlewares"
+	"github.com/CSKU-Lab/main-server/internal/requests"
 	"github.com/gofiber/fiber/v3"
 )
 
 func NewCMSConfigRoutes(router fiber.Router, configGRPCClient configPB.ConfigServiceClient) {
-	configRouter := router.Group("/configs")
+	configRouter := router.Group("/configs", middlewares.RBACMiddleware([]models.Role{
+		models.ADMIN,
+		models.INSTRUCTOR,
+	}))
+
+	configRouter.Post("/runners", middlewares.ValidateMiddleware[requests.CreateRunnerRequest](), func(c fiber.Ctx) error {
+		req := c.Locals("body").(*requests.CreateRunnerRequest)
+		runner, err := configGRPCClient.CreateRunner(c.RequestCtx(), &configPB.CreateRunnerRequest{
+			Name:        req.Name,
+			BuildScript: req.BuildScript,
+			RunScript:   req.RunScript,
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(runner)
+	})
+
+	configRouter.Patch("/runners/:id", middlewares.ValidateMiddleware[requests.UpdateRunnerRequest](), func(c fiber.Ctx) error {
+		req := c.Locals("body").(*requests.UpdateRunnerRequest)
+		id := c.Params("id")
+		runner, err := configGRPCClient.UpdateRunner(c.RequestCtx(), &configPB.UpdateRunnerRequest{
+			Id:          id,
+			Name:        req.Name,
+			BuildScript: req.BuildScript,
+			RunScript:   req.RunScript,
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(runner)
+	})
+
+	configRouter.Delete("/runners/:id", func(c fiber.Ctx) error {
+		id := c.Params("id")
+		_, err := configGRPCClient.DeleteRunner(c.RequestCtx(), &configPB.DeleteRunnerRequest{
+			Id: id,
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(fiber.Map{
+			"message": "Runner deleted successfully",
+		})
+	})
 
 	configRouter.Get("/runners", func(c fiber.Ctx) error {
 		includeScriptQuery := c.Query("include_script", "false")
@@ -119,6 +165,53 @@ func NewCMSConfigRoutes(router fiber.Router, configGRPCClient configPB.ConfigSer
 				"total_rows": paginationRes.Count,
 			},
 			"data": paginationRes.Compares,
+		})
+	})
+
+	configRouter.Post("/compare-scripts", middlewares.ValidateMiddleware[requests.CreateCompareRequest](), func(c fiber.Ctx) error {
+		req := c.Locals("body").(*requests.CreateCompareRequest)
+		compare, err := configGRPCClient.CreateCompare(c.RequestCtx(), &configPB.CreateCompareRequest{
+			Name:        req.Name,
+			RunScript:   req.RunScript,
+			RunName:     req.RunName,
+			Description: req.Description,
+			BuildScript: req.BuildScript,
+			Files:       requests.MapCompareFilesToPB(req.Files),
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(compare)
+	})
+
+	configRouter.Patch("/compare-scripts/:id", middlewares.ValidateMiddleware[requests.UpdateCompareRequest](), func(c fiber.Ctx) error {
+		req := c.Locals("body").(*requests.UpdateCompareRequest)
+		id := c.Params("id")
+		compare, err := configGRPCClient.UpdateCompare(c.RequestCtx(), &configPB.UpdateCompareRequest{
+			Id:          id,
+			Name:        req.Name,
+			RunScript:   req.RunScript,
+			RunName:     req.RunName,
+			Description: req.Description,
+			BuildScript: req.BuildScript,
+			Files:       requests.MapCompareFilesToPB(req.Files),
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(compare)
+	})
+
+	configRouter.Delete("/compare-scripts/:id", func(c fiber.Ctx) error {
+		id := c.Params("id")
+		_, err := configGRPCClient.DeleteCompare(c.RequestCtx(), &configPB.DeleteCompareRequest{
+			Id: id,
+		})
+		if err != nil {
+			return err
+		}
+		return c.JSON(fiber.Map{
+			"message": "Compare deleted successfully",
 		})
 	})
 }
---
+++ internal/requests/configs.go
@@ -0,0 +1,90 @@
+package requests
+
+import (
+	configPB "github.com/CSKU-Lab/main-server/genproto/config/v1"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type CreateRunnerRequest struct {
+	Name        string  `json:"name"`
+	BuildScript *string `json:"build_script"`
+	RunScript   string  `json:"run_script"`
+}
+
+type UpdateRunnerRequest struct {
+	Name        *string `json:"name"`
+	BuildScript *string `json:"build_script"`
+	RunScript   *string `json:"run_script"`
+}
+
+func (cr *CreateRunnerRequest) Validate() error {
+	return validation.ValidateStruct(cr,
+		validation.Field(&cr.Name, validation.Required),
+		validation.Field(&cr.BuildScript, validation.NilOrNotEmpty),
+		validation.Field(&cr.RunScript, validation.Required),
+	)
+}
+
+func (ur *UpdateRunnerRequest) Validate() error {
+	return validation.ValidateStruct(ur,
+		validation.Field(&ur.Name, validation.NilOrNotEmpty),
+		validation.Field(&ur.BuildScript, validation.NilOrNotEmpty),
+		validation.Field(&ur.RunScript, validation.NilOrNotEmpty),
+	)
+}
+
+type CompareFile struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type CreateCompareRequest struct {
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	BuildScript string         `json:"build_script"`
+	RunScript   string         `json:"run_script"`
+	RunName     string         `json:"run_name"`
+	Files       []*CompareFile `json:"files"`
+}
+
+type UpdateCompareRequest struct {
+	Name        *string        `json:"name"`
+	Description *string        `json:"description"`
+	BuildScript *string        `json:"build_script"`
+	RunScript   *string        `json:"run_script"`
+	RunName     *string        `json:"run_name"`
+	Files       []*CompareFile `json:"files"`
+}
+
+func (cc *CreateCompareRequest) Validate() error {
+	return validation.ValidateStruct(cc,
+		validation.Field(&cc.Name, validation.Required),
+		validation.Field(&cc.Description, validation.Required),
+		validation.Field(&cc.BuildScript, validation.Required),
+		validation.Field(&cc.RunScript, validation.Required),
+		validation.Field(&cc.RunName, validation.Required),
+		validation.Field(&cc.Files, validation.NilOrNotEmpty),
+	)
+}
+
+func (uc *UpdateCompareRequest) Validate() error {
+	return validation.ValidateStruct(uc,
+		validation.Field(&uc.Name, validation.NilOrNotEmpty),
+		validation.Field(&uc.Description, validation.NilOrNotEmpty),
+		validation.Field(&uc.BuildScript, validation.NilOrNotEmpty),
+		validation.Field(&uc.RunScript, validation.NilOrNotEmpty),
+		validation.Field(&uc.RunName, validation.NilOrNotEmpty),
+		validation.Field(&uc.Files, validation.NilOrNotEmpty),
+	)
+}
+
+func MapCompareFilesToPB(files []*CompareFile) []*configPB.File {
+	pbFiles := make([]*configPB.File, len(files))
+	for i, file := range files {
+		pbFiles[i] = &configPB.File{
+			Name:    file.Name,
+			Content: file.Content,
+		}
+	}
+	return pbFiles
+}
