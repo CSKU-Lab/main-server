40bc96294b0558da27913aabd0b492724f515ec0
SornchaiTheDev <sornchaithedev@gmail.com>
2026-05-16 19:37:59 +0700

    chore: update trigger statement

--- postgresql/triggers/submission_outbox_trigger.sql
+++ postgresql/triggers/submission_outbox_trigger.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER trigger_code_submissions_outbox_insert
+CREATE OR REPLACE TRIGGER trigger_code_submissions_outbox_insert
 AFTER INSERT ON code_submissions_outbox
 FOR EACH ROW
 EXECUTE FUNCTION code_submissions_outbox_notify();
