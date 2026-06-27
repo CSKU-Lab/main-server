b4d4bf1260eceb37d1d03784bcf3ad2d8545c149
SornchaiTheDev <sornchaithedev@gmail.com>
2026-06-27 16:40:41 +0700

    chore: increase user rate limit

--- internal/providers/app.go
+++ internal/providers/app.go
@@ -120,7 +120,7 @@ func NewFiberApp(
 		TagService:              tagService,
 	})
 
-	coreApi := protectedApi.Group("/", middlewares.RateLimitMiddleware(rateLimiter, 120, time.Minute))
+	coreApi := protectedApi.Group("/", middlewares.RateLimitMiddleware(rateLimiter, 300, time.Minute))
 
 	rest.NewCoreRouter(&rest.CoreRouter{
 		Router:                     coreApi,
